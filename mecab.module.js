b8c91f2768171c57b1f69c094e483e12b37ce19d
DavidK <david.kelver1@gmail.com>
2024-12-20 11:09:27 -0600

    Update mecab.module.js

--- mecab.module.js
+++ mecab.module.js
@@ -3,9 +3,9 @@ import LoadMecab from "https://unpkg.com/mecab-wasm@1.0.3/lib/libmecab.js";
 function locateFile(fn) {
     let url = '';
     if (fn === 'libmecab.data') {
-        url = 'https://unpkg.com/mecab-wasm@1.0.3/lib/libmecab.data';
+        url = 'https://unpkg.com/mecab-wasm@1.0.3/lib/libmecab.data'; // CDN URL for libmecab.data
     } else if (fn === 'libmecab.wasm') {
-        url = 'https://unpkg.com/mecab-wasm@1.0.3/lib/libmecab.wasm';
+        url = 'https://unpkg.com/mecab-wasm@1.0.3/lib/libmecab.wasm'; // CDN URL for libmecab.wasm
     } else {
         return fn;  // Return the default file if it's neither of the two
     }
@@ -13,8 +13,6 @@ function locateFile(fn) {
     console.log(`Resolving ${fn} to: ${url}`);
     return url;  // Return the URL to fetch the file
 }
-
-
 
 var lib;
 var instance;
